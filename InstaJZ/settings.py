4008b81cea3713050cb7e1565989afffbf32e1dd
unknown <liuw888@uchicago.edu>
2019-10-02 15:01:04 -0500

    whitenoise_modified

--- InstaJZ/settings.py
+++ InstaJZ/settings.py
@@ -131,4 +131,6 @@ LOGOUT_REDIRECT_URL = 'posts'
 
 AUTH_USER_MODEL = 'Insta.InstaUser'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressdManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
